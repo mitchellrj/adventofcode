cbf9f5f70ffd0a93cb387855b9c7423050892de4
Richard Mitchell <richard.j.mitchell@gmail.com>
2020-12-02 18:03:57 +0000

    2020-12-01

---
+++ 2020/01/solution1.py
@@ -0,0 +1,43 @@
+import sys
+import time
+
+
+TARGET = 2020
+
+
+def main(expenses):
+    expenses_sorted = sorted(expenses, reverse=True)
+    for i, e in enumerate(expenses_sorted):
+        if e < 2020:
+            break
+    if i:
+        expenses_sorted = sexpenses_sorted[i:]
+    b = 0
+    while True:
+        l = len(expenses_sorted) - 1
+        while True:
+            s = expenses_sorted[b] + expenses_sorted[l]
+            if s == TARGET:
+                return expenses_sorted[l] * expenses_sorted[b]
+            elif s > TARGET:
+                break
+            l -= 1
+        b += 1
+
+
+def reader(fh):
+    for l in fh:
+        yield int(l)
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    with open(fname, 'r') as fh:
+        inputs = list(reader(fh))
+
+    start = time.time()
+    result = main(inputs)
+    end = time.time()
+
+    print(result)
+    print(f'Result calculated in {round(end - start):0.5f} seconds.', file=sys.stderr)
---
+++ 2020/01/solution2.py
@@ -0,0 +1,49 @@
+import sys
+import time
+
+
+TARGET = 2020
+
+
+def main(expenses):
+    expenses_sorted = sorted(expenses, reverse=True)
+    for i, e in enumerate(expenses_sorted):
+        if e < 2020:
+            break
+    if i:
+        expenses_sorted = sexpenses_sorted[i:]
+    b = 0
+    while True:
+        l = len(expenses_sorted)
+        while True:
+            l -= 1
+            m = l
+            while True:
+                m -= 1
+                print(b, m, l)
+                s = expenses_sorted[b] + expenses_sorted[m] + expenses_sorted[l]
+                if s == TARGET:
+                    return expenses_sorted[l] * expenses_sorted[m] * expenses_sorted[b]
+                elif s > TARGET:
+                    break
+            if l <= b or (expenses_sorted[b] + expenses_sorted[l] + 1) > TARGET:
+                break
+        b += 1
+
+
+def reader(fh):
+    for l in fh:
+        yield int(l)
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    with open(fname, 'r') as fh:
+        inputs = list(reader(fh))
+
+    start = time.time()
+    result = main(inputs)
+    end = time.time()
+
+    print(result)
+    print(f'Result calculated in {round(end - start):0.5f} seconds.', file=sys.stderr)
