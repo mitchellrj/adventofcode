e04658f51cb43ec2e7583165f26a6c314e281ea0
Richard Mitchell <richard.j.mitchell@gmail.com>
2020-12-06 10:18:09 +0000

    2020-12-06

---
+++ 2020/06/solution1.py
@@ -0,0 +1,31 @@
+import sys
+import time
+
+
+def main(yesses):
+    return sum(map(len, yesses))
+
+
+def reader(fh):
+    yesses = set()
+    for l in fh:
+        l = l.strip()
+        if not l:
+            yield yesses
+            yesses = set()
+        yesses = yesses | set(l)
+
+    yield yesses
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    with open(fname, 'r') as fh:
+        inputs = list(reader(fh))
+
+    start = time.monotonic_ns()
+    result = main(inputs)
+    end = time.monotonic_ns()
+
+    print(result)
+    print(f'Result calculated in {(end - start) / 1e3:0.3f} microseconds.', file=sys.stderr)
---
+++ 2020/06/solution2.py
@@ -0,0 +1,39 @@
+import sys
+import time
+
+
+ALL_QUESTIONS = frozenset(map(chr, range(97, 123)))
+
+
+def main(yesses):
+    return sum(map(len, yesses))
+
+
+def reader(fh):
+    new_group = True
+    for l in fh:
+        l = l.strip()
+        if not l:
+            yield yesses
+            new_group = True
+            continue
+        if new_group:
+            yesses = set(l)
+            new_group = False
+        else:
+            yesses = yesses & set(l)
+
+    yield yesses
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    with open(fname, 'r') as fh:
+        inputs = list(reader(fh))
+
+    start = time.monotonic_ns()
+    result = main(inputs)
+    end = time.monotonic_ns()
+
+    print(result)
+    print(f'Result calculated in {(end - start) / 1e3:0.3f} microseconds.', file=sys.stderr)
